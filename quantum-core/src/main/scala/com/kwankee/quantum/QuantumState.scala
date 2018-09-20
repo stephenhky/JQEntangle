b59bacedc48b0ab4c1756863ea8f857749b75174
stephenhky <stephenhky@yahoo.com.hk>
2018-09-19 23:05:15 -0400

    defining quantum state

--- quantum-core/src/main/scala/com/kwankee/quantum/QuantumState.scala
+++ quantum-core/src/main/scala/com/kwankee/quantum/QuantumState.scala
@@ -1,5 +1,17 @@
 package com.kwankee.quantum
 
-class QuantumState(nbStates : Int = 2) {
+import org.nd4j.linalg.factory.Nd4j
 
+class QuantumState(nbStates : Int = 2) {
+  var states = Nd4j.rand(nbStates, 1)
+
+  def norm() : Double =
+    Math.sqrt(Range(0, nbStates)
+      .map(i => states.getDouble(i, 0)*states.getDouble(i, 0))
+      .reduce((a, b) => a+b))
+
+  def normalize() : Unit = {
+    val norm = norm()
+    Range(0, nbStates).foreach(i => states.putScalar(i, 0, states.getDouble(i, 0) / norm))
+  }
 }
